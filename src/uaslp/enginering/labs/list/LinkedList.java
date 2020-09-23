c33419858698ba1853154a59d00902cd2106c951
TonyGalvan <tonygalvan010@gmail.com>
2020-09-22 23:42:59 -0500

    Estructura LinkedList creada

---
+++ src/uaslp/enginering/labs/list/LinkedList.java
@@ -0,0 +1,40 @@
+package uaslp.enginering.labs.list;
+
+
+public class LinkedList {
+
+    //Atributos
+    private Node front;
+    private Node tail;
+
+
+    //Métodos
+    public void add(Student student){
+
+    }
+
+    public void delete(Student student){
+
+    }
+
+    public void delete(int index){
+
+    }
+
+    public IteratorArrayList getIterator(){
+        return null;
+    }
+
+    public int size(){
+        return 0;
+    }
+
+    public Student getAt(int index){
+        return null;
+    }
+
+    public void insert(Student reference, Student newStudent){
+
+    }
+
+}
